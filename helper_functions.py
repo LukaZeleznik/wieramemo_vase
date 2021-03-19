c401720061cfa25ab0ca96c762d9e1b8f18d7ea2
Luka Železnik <luka.zeleznik10@gmail.com>
2021-03-19 15:24:46 +0100

    dbdbdbdbdbdb

--- helper_functions.py
+++ helper_functions.py
@@ -4,6 +4,7 @@ import psycopg2
 DBNAME = "wier"
 USER = "postgres"
 PASSWORD = "admin"
+CONN_DATA = "dbname="+DBNAME+" user="+USER+" password="+PASSWORD
 
 def append_to_file(path, data):
     with open(path, 'a') as file:
@@ -42,20 +43,111 @@ def set_to_file(links, file):
     for link in sorted(links):
         append_to_file(file, link)
 
-def insert_site(id, domain, robots, sitemap):
-    conn = psycopg2.connect("dbname="+DBNAME+" user="+USER+" password="+PASSWORD)
+def insert_site(domain, robots, sitemap):
+    conn = psycopg2.connect(CONN_DATA)
     cur = conn.cursor()
 
-    query = "insert into crawldb.site(id, domain, robots_content, sitemap_content) VALUES ("+str(id)+", '"+domain+"', '"+robots+"', '"+sitemap+"')"
+    query = "insert into crawldb.site(domain, robots_content, sitemap_content) VALUES ('"+domain+"', '"+robots+"', '"+sitemap+"') RETURNING *;"
 
     cur.execute(query)
+    insertedid = cur.fetchone()
     conn.commit()
     cur.close()
 
+    return insertedid
+
 def get_sites():
-    conn = psycopg2.connect("dbname="+DBNAME+" user="+USER+" password="+PASSWORD)
+    conn = psycopg2.connect(CONN_DATA)
     cur = conn.cursor()
     query = "SELECT * FROM crawldb.site"
+    cur.execute(query)
+    wier = cur.fetchall()
+    cur.close()
+    return wier
+
+def insert_page(site_id, page_type_code, url, html_content, http_status_code, accessed_time):
+    conn = psycopg2.connect(CONN_DATA)
+    cur = conn.cursor()
+
+    query = "insert into crawldb.page(site_id, page_type_code, url, html_content, http_status_code, accessed_time) VALUES ('"+str(site_id)+"', '"+page_type_code+"', '"+url+"', '"+html_content+"', '"+http_status_code+"', '"+accessed_time+"') RETURNING *;"
+
+    cur.execute(query)
+    insertedid = cur.fetchone()
+    conn.commit()
+    cur.close()
+
+    return insertedid
+
+def get_pages():
+    conn = psycopg2.connect(CONN_DATA)
+    cur = conn.cursor()
+    query = "SELECT * FROM crawldb.page"
+    cur.execute(query)
+    wier = cur.fetchall()
+    cur.close()
+    return wier
+
+def insert_image(page_id, filename, content_type, data, accessed_time):
+    conn = psycopg2.connect(CONN_DATA)
+    cur = conn.cursor()
+
+    query = "insert into crawldb.image(page_id, filename, content_type, data, accessed_time) VALUES ('"+str(page_id)+"', '"+filename+"', '"+content_type+"', '"+str(data)+"', '"+accessed_time+"') RETURNING *;"
+
+    cur.execute(query)
+    insertedid = cur.fetchone()
+    conn.commit()
+    cur.close()
+
+    return insertedid
+
+def get_images():
+    conn = psycopg2.connect(CONN_DATA)
+    cur = conn.cursor()
+    query = "SELECT * FROM crawldb.image"
+    cur.execute(query)
+    wier = cur.fetchall()
+    cur.close()
+    return wier
+
+def insert_page_data(page_id, data_type_code, data):
+    conn = psycopg2.connect(CONN_DATA)
+    cur = conn.cursor()
+
+    query = "insert into crawldb.page_data(page_id, data_type_code, data) VALUES ('"+str(page_id)+"', '"+data_type_code+"', '"+str(data)+"') RETURNING *;"
+
+    cur.execute(query)
+    insertedid = cur.fetchone()
+    conn.commit()
+    cur.close()
+
+    return insertedid
+
+def get_page_data():
+    conn = psycopg2.connect(CONN_DATA)
+    cur = conn.cursor()
+    query = "SELECT * FROM crawldb.page_data"
+    cur.execute(query)
+    wier = cur.fetchall()
+    cur.close()
+    return wier
+
+def insert_link(from_page_id, to_page_id):
+    conn = psycopg2.connect(CONN_DATA)
+    cur = conn.cursor()
+
+    query = "insert into crawldb.link(from_page, to_page) VALUES ('"+str(from_page_id)+"', '"+str(to_page_id)+"') RETURNING *;"
+
+    cur.execute(query)
+    insertedid = cur.fetchone()
+    conn.commit()
+    cur.close()
+
+    return insertedid
+
+def get_links():
+    conn = psycopg2.connect(CONN_DATA)
+    cur = conn.cursor()
+    query = "SELECT * FROM crawldb.link"
     cur.execute(query)
     wier = cur.fetchall()
     cur.close()
