f2b72ad2dcfd9fedc11e8917dd91611c5ea499c5
zigakleine <ziga.kleine@gmail.com>
2021-04-20 19:37:39 +0200

    Dodal xpath za overstock.

--- pa2/implementation-extraction/xpath-extraction.py
+++ pa2/implementation-extraction/xpath-extraction.py
@@ -1,10 +1,57 @@
 import codecs
 from lxml import html
+import re
 
-def extract_with_xpath(page_html):
+def extract_with_xpath_overstock(page_html):
 
-    # title = listPrice = price = saving = savingPercent = content = ""
+    title = list_price = price = saving = saving_percent = content = ""
+
+    document_tree = html.fromstring(page_html)
+
+    titles = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/a/b/text()')
+    print("titles: (", len(titles), ")")
+    for title in titles:
+        print("\t", title)
+
+    list_prices = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/s/text()')
+
+    print("list prices: (", len(list_prices), ")")
+    for list_price in list_prices:
+        print("\t", list_price)
+
+    prices = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/span/b/text()')
+
+    print("prices: (", len(prices), ")")
+    for price in prices:
+        print("\t", price)
+
+
+    savings_and_savings_percents = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/span/text()')
+
+    print("savings: (", len(savings_and_savings_percents), ")")
+    for savings_and_saving_percent in savings_and_savings_percents:
+
+        saving = re.findall("^\S*", savings_and_saving_percent)
+        saving_percent = re.findall("\((.*?)\)", savings_and_saving_percent)
+
+        print("\t", saving[0], "---", saving_percent[0])
+
+
+    contents = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/table/tbody/tr/td[2]')
+    contents = [content.text_content() for content in contents]
+
+    print("contents: (", len(contents), ")")
+    for content in contents:
+
+
+        print("\t", content)
+
+    return ""
+
+def extract_with_xpath_rtv(page_html):
+
     author = published_time = title = sub_title = lead = content = ""
+
     document_tree = html.fromstring(page_html)
 
     author = document_tree.xpath('//div[@class="author-timestamp"]/strong/text()')
@@ -31,7 +78,9 @@ def extract_with_xpath(page_html):
 
 
 if __name__ == "__main__":
-    f = codecs.open("../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html", 'r')
+    # f = codecs.open("../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html", 'r')
+    f = codecs.open("../input-extraction/overstock.com/jewelry01.html", 'r')
     page_html = f.read()
-    extracted_data = extract_with_xpath(page_html)
-    #print(extracted_data)
+    # extracted_data = extract_with_xpath_rtv(page_html)
+    extracted_data = extract_with_xpath_overstock(page_html)
+    # print(extracted_data)
