7417f9067e27ac0f884d89351c02537e6a40daf0
zigakleine <ziga.kleine@gmail.com>
2021-04-20 21:13:16 +0200

    xpath extraction on both pages done.

--- pa2/implementation-extraction/xpath-extraction.py
+++ pa2/implementation-extraction/xpath-extraction.py
@@ -1,86 +1,135 @@
 import codecs
 from lxml import html
 import re
+import json
 
 def extract_with_xpath_overstock(page_html):
 
-    title = list_price = price = saving = saving_percent = content = ""
+    # title = list_price = price = saving = saving_percent = content = ""
 
     document_tree = html.fromstring(page_html)
 
     titles = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/a/b/text()')
-    print("titles: (", len(titles), ")")
-    for title in titles:
-        print("\t", title)
+    titles = [" ".join(title.replace("\n", " ").split()) for title in titles]
+
+    # print("titles: (", len(titles), ")")
+    # for title in titles:
+    #     print("\t", title)
 
     list_prices = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/s/text()')
 
-    print("list prices: (", len(list_prices), ")")
-    for list_price in list_prices:
-        print("\t", list_price)
+    # print("list prices: (", len(list_prices), ")")
+    # for list_price in list_prices:
+    #     print("\t", list_price)
 
     prices = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/span/b/text()')
 
-    print("prices: (", len(prices), ")")
-    for price in prices:
-        print("\t", price)
+    # print("prices: (", len(prices), ")")
+    # for price in prices:
+    #     print("\t", price)
 
 
     savings_and_savings_percents = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/span/text()')
+    savings = []
+    savings_percents = []
 
-    print("savings: (", len(savings_and_savings_percents), ")")
+    # print("savings: (", len(savings_and_savings_percents), ")")
     for savings_and_saving_percent in savings_and_savings_percents:
 
-        saving = re.findall("^\S*", savings_and_saving_percent)
-        saving_percent = re.findall("\((.*?)\)", savings_and_saving_percent)
+        saving = re.findall("^\S*", savings_and_saving_percent)[0]
+        savings.append(saving)
+        saving_percent = re.findall("\((.*?)\)", savings_and_saving_percent)[0]
+        savings_percents.append(saving_percent)
 
-        print("\t", saving[0], "---", saving_percent[0])
+        # print("\t", saving, "---", saving_percent)
 
 
     contents = document_tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[*]/td[2]/table/tbody/tr/td[2]')
-    contents = [content.text_content() for content in contents]
+    contents = [" ".join(content.text_content().replace("\n", " ").split()) for content in contents]
 
-    print("contents: (", len(contents), ")")
-    for content in contents:
+    # print("contents: (", len(contents), ")")
+    # for content in contents:
+    #     print("\t", content)
 
+    data_records = []
 
-        print("\t", content)
+    for title, list_price, price, saving, saving_percent, content in zip(titles, list_prices, prices, savings,
+                                                                         savings_percents, contents):
+        data_record = dict()
+        data_record["Title"] = title
+        data_record["ListPrice"] = list_price
+        data_record["Price"] = price
+        data_record["Saving"] = saving
+        data_record["SavingPercent"] = saving_percent
+        data_record["Content"] = content
 
-    return ""
+        data_records.append(data_record)
+
+    json_data_records = json.dumps(data_records, ensure_ascii=False)
+    return json_data_records
 
 def extract_with_xpath_rtv(page_html):
 
-    author = published_time = title = sub_title = lead = content = ""
+    # author = published_time = title = sub_title = lead = content = ""
 
     document_tree = html.fromstring(page_html)
 
     author = document_tree.xpath('//div[@class="author-timestamp"]/strong/text()')
-    print("author:", author[0])
+    author = author[0]
+    # print("author:", author)
 
     title = document_tree.xpath('//header[@class="article-header"]/h1/text()')
-    print("title:", title[0])
+    title = title[0]
+    # print("title:", title)
 
     published_time = document_tree.xpath('//div[@class="author-timestamp"]/text()')
-    print("published time:", published_time[1].strip("| "))
+    published_time = published_time[1].replace("|", "").replace("\t", "").replace("\n", "").strip()
+    # print("published time:", published_time)
 
     sub_title = document_tree.xpath('//div[@class="subtitle"]/text()')
-    print("sub title:", sub_title[0])
+    sub_title = sub_title[0]
+    # print("sub title:", sub_title)
 
     lead = document_tree.xpath('//p[@class="lead"]/text()')
-    print("lead:", lead[0])
+    lead = lead[0]
+    # print("lead:", lead)
 
     content = document_tree.xpath('//article[@class="article"]/p')
     content = [p.text_content() for p in content]
     separator = " "
-    print("content: ", separator.join(content))
+    content = separator.join(content).strip()
+    # print("content: ", content)
 
-    return ""
+    data_record = dict()
+
+    data_record["Author"] = author
+    data_record["Title"] = title
+    data_record["PublishedTime"] = published_time
+
+    data_record["SubTitle"] = sub_title
+    data_record["Lead"] = lead
+    data_record["Content"] = content
+
+    json_data_record = json.dumps(data_record, ensure_ascii=False)
+    return json_data_record
 
 
 if __name__ == "__main__":
-    # f = codecs.open("../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html", 'r')
-    f = codecs.open("../input-extraction/overstock.com/jewelry01.html", 'r')
-    page_html = f.read()
-    # extracted_data = extract_with_xpath_rtv(page_html)
-    extracted_data = extract_with_xpath_overstock(page_html)
-    # print(extracted_data)
+
+    rtv_html_names = ["Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html",
+                 "Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html" ]
+
+    overstock_html_names = ["jewelry01.html", "jewelry02.html"]
+
+    for rtv_html_name in rtv_html_names:
+        f = codecs.open("../input-extraction/rtvslo.si/" + rtv_html_name, 'r', encoding='utf-8')
+        page_html = f.read()
+        extracted_data = extract_with_xpath_rtv(page_html)
+        print(rtv_html_name + ":", extracted_data)
+
+    for overstock_html_name in overstock_html_names:
+        f = codecs.open("../input-extraction/overstock.com/" + overstock_html_name, 'r', encoding='iso-8859-1',)
+        page_html = f.read()
+        extracted_data = extract_with_xpath_overstock(page_html)
+        print(overstock_html_name + ":", extracted_data)
+
