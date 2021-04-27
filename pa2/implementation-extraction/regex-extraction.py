cada3b1f0659be2e50e3a3db2385fc9946492136
zigakleine <ziga.kleine@gmail.com>
2021-04-27 22:36:03 +0200

    directory fix (upam)

--- pa2/implementation-extraction/regex-extraction.py
+++ pa2/implementation-extraction/regex-extraction.py
@@ -1,4 +1,5 @@
 import re, os, sys, codecs
+import os
 
 def extract_with_regex_overstock(text):
     titles = []
@@ -190,8 +191,11 @@ if __name__ == "__main__":
         for data in extracted_data:
             print(overstock_html_name + ":", data) """
 
+
+    print()
+
     for imdb_html_name in imdb_html_names:
-        f = codecs.open(os.path.join(os.getcwd(), 'pa2', 'input-extraction', 'imdb.com', imdb_html_name), 'r')
+        f = codecs.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',  'input-extraction', 'imdb.com', imdb_html_name), 'r')
         page_html = f.read()
         extracted_data = extract_with_regex_imdb(page_html)
         for data in extracted_data:
